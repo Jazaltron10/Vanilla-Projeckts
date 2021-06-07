7014c02c3fa3c27ac43160837ae119a1d6994715
Jazaltron <jazaltron10@gmail.com>
2021-06-07 07:25:02 +0100

    Started a New project called Review Page

---
+++ Project 10 - Reviews/Rev.js
@@ -0,0 +1,6 @@
+// document.write('january')
+
+// main = document.querySelector('main')
+// main.textContent = "Don't mind these guys"
+// main.style.backgroundColor = 'green'
+// main.style.fontSize = '3rem'

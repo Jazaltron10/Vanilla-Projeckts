09d92a30caec64770c6a27410118a390c084f231
Jazaltron <jazaltron10@gmail.com>
2021-06-11 20:33:42 +0100

    Just renamed a couple of files

--- Project 10 - Reviews/Rev.js
+++
@@ -1,6 +0,0 @@
-// document.write('january')
-
-// main = document.querySelector('main')
-// main.textContent = "Don't mind these guys"
-// main.style.backgroundColor = 'green'
-// main.style.fontSize = '3rem'

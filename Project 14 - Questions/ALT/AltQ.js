5a678a113c76424dd706de3aed493d39064b7341
Jazaltron <jazaltron10@gmail.com>
2021-06-23 18:40:50 +0100

    Few MOdifications

---
+++ Project 14 - Questions/ALT/AltQ.js
@@ -0,0 +1,10 @@
+// first option - traversing the DOM
+// second option - using selectors inside the element 
+
+//FO
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach((btn)=>{
+    
+})

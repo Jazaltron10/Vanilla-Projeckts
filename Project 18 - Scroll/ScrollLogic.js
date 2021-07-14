8348e91804268878ae38d56fb9e4ca4b9dba091a
Jazaltron <jazaltron10@gmail.com>
2021-07-14 20:09:16 +0100

    Made significant progress in Scroll Project

---
+++ Project 18 - Scroll/ScrollLogic.js
@@ -0,0 +1,9 @@
+const togglebtn = document.querySelector("#toggle");
+const navlinks = document.querySelector("#navlinks");
+
+
+
+togglebtn.addEventListener('click', function(){
+    console.log("hello")
+    navlinks.classList.toggle("show__nav");
+})

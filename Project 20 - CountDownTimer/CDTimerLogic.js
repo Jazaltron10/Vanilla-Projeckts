07090b9efcdfb1386a918e415acc31638f755f74
Jazaltron <jazaltron10@gmail.com>
2021-08-24 13:41:49 +0100

    Started the Countdown deadline Project, Finished styling, adding logic

---
+++ Project 20 - CountDownTimer/CDTimerLogic.js
@@ -0,0 +1,80 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".numval");
+
+
+// year, month, date, hours, minutes, seconds
+let futureDate = new Date(2020, 11, 24, 11, 30, 0);
+const year = futureDate.getFullYear()
+const hours = futureDate.getHours()
+const minutes = futureDate.getMinutes()
+
+let month = futureDate.getMonth()
+month = months[month]
+
+const date  = futureDate.getDate()
+const weekday = weekdays[futureDate.getDay()];
+
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes} am `
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
